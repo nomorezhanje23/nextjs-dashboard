c47d114ad6caf9fdf69dc6a223801fea887f0fef
nomorezhanje23 <nomorezhanje23@gmail.com>
2024-10-10 23:51:27 +0200

    Revert "updated files"
    This reverts commit 9ac81e9d675c8788cc035588517a3db1176ee17a.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,33 +3,23 @@ import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
-import { Metadata } from 'next';
  
-export const metadata: Metadata = {
-  title: 'Invoices',
-};
- 
-export default async function Page({ searchParams }: {searchParams?: {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
     query?: string;
     page?: string;
-    };
+  };
 }) {
-    // const query = searchParams?.query || '1';
-    const query = typeof searchParams?.query === 'string' ? searchParams?.query : '';
-    const currentPage = Number(searchParams?.page) || 1;
-    let numberQuery;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
 
-    // Check if query can be converted to a number and assign it to numberQuery
-    if (typeof searchParams?.query !== 'string' || query === '') {
-      numberQuery = 1; // Or a different default value
-    } else {
-      numberQuery = parseInt(query, 10);
-    }
-
-    const totalPages = await fetchInvoicesPages(query);
+  const totalPages = await fetchInvoicesPages(query);
+ 
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -39,7 +29,7 @@ export default async function Page({ searchParams }: {searchParams?: {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={numberQuery + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
